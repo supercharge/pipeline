265a136dcf40ff2fbc70b429f26201dc068d41de
Marcus Poehls <marcus.poehls@gmail.com>
2020-02-15 14:54:52 +0100

    add tests

--- test/index.js
+++ test/index.js
@@ -12,28 +12,94 @@ describe('Pipeline', () => {
     called = false
   })
 
-  it('runs a basic pipeline', async () => {
+  it('runs a basic array pipeline', async () => {
     await Pipeline
-      .send('hello')
+      .send('Marcus')
       .through([
+        PipelineTask,
+        function inlinePipelineFunc (input) {
+          return `${input}-Function`
+        }
+      ])
+      .then(result => {
+        expect(called).to.be.true()
+        expect(result).to.equal('Marcus-Class-Function')
+      })
+  })
+
+  it('runs a basic items pipeline', async () => {
+    const result = await Pipeline
+      .send('hello')
+      .through(
         PipelineTask,
         function inlinePipelineFunc (input) {
           return input
         }
-      ])
+      )
+      .then(result => {
+        return `${result}-finished`
+      })
+
+    expect(called).to.be.true()
+    expect(result).to.equal('hello-Class-finished')
+  })
+
+  it('thenReturn', async () => {
+    const result = await Pipeline
+      .send('hello')
+      .through(
+        PipelineTask,
+        function inlinePipelineFunc () {
+          return 'this is the final result'
+        }
+      )
+      .thenReturn()
+
+    expect(called).to.be.true()
+    expect(result).to.equal('this is the final result')
+  })
+
+  it('via', async () => {
+    await Pipeline
+      .send('hello')
+      .via('customMethodName')
+      .through(
+        CustomMethodPipelineTask
+      )
       .then(result => {
         expect(called).to.be.true()
         expect(result).to.equal('hello')
       })
   })
+
+  it('throws when neither class nor function', async () => {
+    await expect(Pipeline
+      .send('hello')
+      .via('customMethodName')
+      .through(123)
+      .then(() => { })
+    ).to.reject()
+  })
 })
 
 class PipelineTask {
+  constructor (name) {
+    this.name = name
+  }
+
+  async handle () {
+    called = true
+
+    return `${this.name}-Class`
+  }
+}
+
+class CustomMethodPipelineTask {
   constructor (params) {
     this.params = params
   }
 
-  async handle () {
+  async customMethodName () {
     called = true
 
     return this.params
